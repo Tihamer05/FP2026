ca726fa8edf0cd986c47ef52abd7295b456ec95e
MezofiTihamer <mtihamer123@gmail.com>
2026-02-22 13:17:38 +0200

    Lab01 - HomeWork

--- 01.labor/lab1_C.hs
+++ 01.labor/lab1_C.hs
@@ -1,3 +1,5 @@
+import Data.Char
+
 -- két szám összegét, különbségét, szorzatát, hányadosát, osztási maradékát
 
 osszeg :: Num a => a -> a -> a
@@ -81,3 +83,84 @@ fakt3 res n
     | otherwise =  fakt3 (res * n) (n - 1)
 
 -- az x szám n-ik hatványát, ha a kitevő pozitív szám (3 módszer)
+
+hatvany1 :: (Floating a) => a -> a-> a
+hatvany1 x n = x ** n 
+
+hatvany2 :: (Num a, Integral b) => a -> b -> a
+hatvany2 x n
+    | n == 0 = 1
+    | otherwise = x * hatvany2 x (n-1)
+
+hatvany3 :: (Num a, Integral b) => a -> b -> a
+hatvany3 _ 0 = 1
+hatvany3 x n
+    | n `mod` 2 == 0 = fel * fel
+    | otherwise = fel * fel * x
+    where
+        fel = hatvany3 x (n `div` 2)
+
+-- az első n természetes szám negyzetgyökét
+
+negyzetgyok :: (Floating a) => Int -> [a]
+negyzetgyok n = [sqrt (fromIntegral x) | x <- [0..(n-1)]]
+
+-- az első n négyzetszámot
+
+negyzetszam :: (Integral a, Enum a) => a -> [a]
+negyzetszam n = [x ^ 2 | x <- [1..(n-1)]]
+
+-- az első n természetes szám köbét
+
+kobszam :: (Integral a) => a -> [a]
+kobszam n = [x ^ 3 | x <- [1..(n-1)]]
+
+-- az első n olyan természetes számot, amelyben nem szerepelnek a négyzetszámok
+
+nemnegyzetszam :: (Integral a, Enum a) => a -> [a]
+nemnegyzetszam n = [x | x <- [1..(n-1)], not (x `elem` [y^2 | y <- [1..(n-1)]])]
+
+-- x hatványait adott n-ig
+
+szam_hatvany :: (Num a, Enum a) => a -> a -> [a]
+szam_hatvany x n = [x * k | k <- [1..n]]
+
+-- egy szám páros osztóinak listáját
+
+paros_osztok :: Integral a => a -> [a]
+paros_osztok x = [nr | nr <- [1..x], x `mod` nr == 0, nr `mod` 2 == 0]
+
+-- prim szam ellenorzes
+prim :: Integral a => a -> Bool
+prim n
+    | n < 2 = False
+    | otherwise = null [d | d <- [2..(n-1)], n `mod` d == 0]
+
+-- n-ig a prímszámok listáját
+
+prim_szamok :: Int -> [Int]
+prim_szamok n = take n [x | x <- [2..], prim x]
+
+-- n-ig az összetett számok listáját
+
+osszetett_szamok :: Int -> [Int]
+osszetett_szamok n = take n [x | x <- [0..], prim x == False]
+
+-- n-ig a páratlan összetett számok listáját
+
+paratlan_osszetett :: Int -> [Int]
+paratlan_osszetett n = take n [ x | x <- [0..], prim x == False, x `mod` 2 == 1]
+
+-- az n-nél kisebb Pitágorászi számhármasokat
+
+pitagoraszi_szamparok :: Int -> [(Int,Int,Int)]
+pitagoraszi_szamparok n = [(a, b, c) | a <- [1..n], b <- [a..n], c <- [b..n], a^2 + b^2 == c^2]
+
+-- Szam es betuparosok
+
+lista = [(chr(i + 97), i) | i <- [0..25]]
+lista_ = zip ['a'..'z'] [1..26]
+
+-- Szamparok lista
+
+lista_nr n = zip [0..n] [n,(n-1)..0]
